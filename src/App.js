72c35276c43fb9d447e77412c8cc7df24ecfa089
Karyn Huston <huston.karyn@gmail.com>
2023-02-01 20:16:28 -0500

    routing for app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Edit from "./Pages/Edit";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/snacks" element={<Index />}/>
+            <Route path="/snacks/new" element={<New />} />
+            <Route path="snack/:id/>" element={<Show />} />
+            <Route path="/snacks/:id/edit" element={<Edit />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
-
-export default App;
